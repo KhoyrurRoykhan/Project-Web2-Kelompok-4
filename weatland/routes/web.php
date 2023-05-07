1004f70cc61faa5afccf073f876a92d542582f13
Srefaldi <2110131110004@mhs.ulm.ac.id>
2023-05-07 17:24:20 +0800

    Login/Register/Logout

--- weatland/routes/web.php
+++ weatland/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Home;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('/home', Home::class);
+Route::get('/sesi', [SessionController::class, 'index']);
+Route::post('/sesi/login', [SessionController::class, 'login']);
+
+Route::get('/sesi/logout', [SessionController::class, 'logout']);
+
+Route::get('/sesi/register', [SessionController::class, 'register']);
+Route::post('/sesi/create', [SessionController::class, 'create']);
